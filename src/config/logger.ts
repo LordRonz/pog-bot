4c0d023da6351f165507b939c02ce695260977c4
LordRonz <aarontanhar2000@gmail.com>
2022-01-29 23:43:20 +0700

    feat: add chalk to color log message

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,8 +1,26 @@
+import chalk from 'chalk';
 import winston from 'winston';
 
 const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
-  format: winston.format.printf((log) => `[${log.level.toUpperCase()}] - ${log.message}`),
+  format: winston.format.printf((log) => {
+    let level = log.level.toUpperCase();
+    // TODO Add coloring for another levels https://github.com/winstonjs/winston#logging-levels
+    switch (level) {
+      case 'INFO':
+        level = chalk.green(level);
+        break;
+      case 'WARN':
+        level = chalk.yellow(level);
+        break;
+      case 'ERROR':
+        level = chalk.red(level);
+        break;
+      default:
+        break;
+    }
+    return `[${level}] - ${log.message}`;
+  }),
 });
 
 export default logger;
