75f54442ca5bce7401005bc6ccbbc9fdda44319a
LordRonz <aarontanhar2000@gmail.com>
2022-02-08 23:48:53 +0700

    feat: remove chalk

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,26 +1,16 @@
-import chalk from 'chalk';
 import winston from 'winston';
 
 const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
-  format: winston.format.printf((log) => {
-    let level = log.level.toUpperCase();
-    // TODO Add coloring for another levels https://github.com/winstonjs/winston#logging-levels
-    switch (level) {
-      case 'INFO':
-        level = chalk.green(level);
-        break;
-      case 'WARN':
-        level = chalk.yellow(level);
-        break;
-      case 'ERROR':
-        level = chalk.red(level);
-        break;
-      default:
-        break;
-    }
-    return `[${level}] - ${log.message}`;
-  }),
+  format: winston.format.combine(
+    winston.format.colorize({
+      all: true,
+    }),
+    winston.format.timestamp({
+      format: 'DD-MM-YY SS:MM:HH',
+    }),
+    winston.format.printf(({ level, message, timestamp }) => `[${timestamp}] [${level.toUpperCase()}] - ${message}`)
+  ),
 });
 
 export default logger;
