26f5499ca3d9dad65dde52f55412a7d9fd6ad49f
LordRonz <aarontanhar2000@gmail.com>
2022-02-27 13:19:59 +0700

    feat: add rps command :rocket:

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,7 +2,8 @@ import anime from './anime';
 import github from './github';
 import help from './help';
 import listguild from './listGuild';
+import rps from './rps';
 import userinfo from './userInfo';
 import whatanime from './whatAnime';
 
-export { anime, github, help, listguild, userinfo, whatanime };
+export { anime, github, help, listguild, rps, userinfo, whatanime };
