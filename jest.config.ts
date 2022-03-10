2000a3de7bd4e3d6d0394d3188a15447dba44366
LordRonz <aarontanhar2000@gmail.com>
2022-03-10 10:09:40 +0700

    refactor: jest.config.ts :rocket:

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
+
+export default config;
